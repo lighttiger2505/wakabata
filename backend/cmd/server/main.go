2f34a1c7d22eddc44731c725079843d59b568849
lighttiger2505 <lighttiger2505@gmail.com>
2024-07-27 16:06:15 +0900

    feat(server): Implement Wakabata RPC service
    The main server file has been updated to implement the Wakabata RPC service. The echo framework has been replaced with a custom server and handler. The server now uses HTTP/2 with the h2c handler.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"connectrpc.com/connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+
+	wakabatav1 "github.com/lighttiger2505/wakabata/internal/app/v1"
+	"github.com/lighttiger2505/wakabata/internal/app/v1/wakabatav1connect"
 )
 
-func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+type WakabataServer struct{}
+
+func (s *WakabataServer) Wakabata(
+	ctx context.Context,
+	req *connect.Request[wakabatav1.WakabataRequest],
+) (*connect.Response[wakabatav1.WakabataResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&wakabatav1.WakabataResponse{
+		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
+func main() {
+	server := &WakabataServer{}
+	mux := http.NewServeMux()
+	path, handler := wakabatav1connect.NewWakabataServiceHandler(server)
+	mux.Handle(path, handler)
+	err := http.ListenAndServe(
+		"localhost:8080",
+		// Use h2c so we can serve HTTP/2 without TLS.
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
