276734d35b980218db740ec7f162d6acf62f97f1
lighttiger2505 <lighttiger2505@gmail.com>
2024-07-27 17:17:39 +0900

    refactor: remove Wakabata service
    The Wakabata service and related files have been removed.
    This includes the main server file, the protobuf definitions,
    and the connect service handler.
    This is part of a larger effort to simplify the backend structure.

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,42 +1,18 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
 
-	"connectrpc.com/connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 
 	"github.com/lighttiger2505/wakabata/internal/app"
-	wakabatav1 "github.com/lighttiger2505/wakabata/internal/app/v1"
 	"github.com/lighttiger2505/wakabata/internal/app/v1/wakabatav1connect"
 )
 
-type WakabataServer struct{}
-
-func (s *WakabataServer) Wakabata(
-	ctx context.Context,
-	req *connect.Request[wakabatav1.WakabataRequest],
-) (*connect.Response[wakabatav1.WakabataResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&wakabatav1.WakabataResponse{
-		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-	})
-	res.Header().Set("Greet-Version", "v1")
-	return res, nil
-}
-
 func main() {
 	mux := http.NewServeMux()
-
-	{
-		svc := &WakabataServer{}
-		path, handler := wakabatav1connect.NewWakabataServiceHandler(svc)
-		mux.Handle(path, handler)
-	}
 
 	{
 		svc := app.NewUserHandler()
