bd7bc0366eb73227a832e56b3989435ebf00f51c
lighttiger2505 <lighttiger2505@gmail.com>
2024-07-27 15:18:13 +0900

    feat: Improve linting and user service
    - Add golangci-lint to Makefile for better code quality
    - Refactor UserService, remove gorm dependency
    - Simplify Create and Update methods in UserService

--- backend/internal/domain/service/user.go
+++ backend/internal/domain/service/user.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/lighttiger2505/WakabaTasks/backend/internal/domain/model"
 	"github.com/lighttiger2505/WakabaTasks/backend/internal/infra"
-	"gorm.io/gorm"
 )
 
 type UserService struct {
@@ -18,20 +17,15 @@ func NewUserService() *UserService {
 
 func (s *UserService) Create(ctx context.Context) error {
 	user := &model.User{}
-	if err := s.UserInfra.CreateUser(ctx, user); err != nil {
+	if err := s.UserInfra.Create(ctx, user); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *UserService) Update() error {
-	user := model.User{
-		Model:        gorm.Model{},
-		Username:     "",
-		Email:        "",
-		PasswordHash: "",
-	}
-	if err := s.UserInfra.CreateUser(ctx, user); err != nil {
+func (s *UserService) Update(ctx context.Context) error {
+	user := &model.User{}
+	if err := s.UserInfra.Update(ctx, user); err != nil {
 		return err
 	}
 	return nil
