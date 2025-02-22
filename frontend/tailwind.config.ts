67cf74a650fddf6918c181333ac8875bd2ded3c9
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-22 12:18:42 +0900

    feat: add Catppuccin theme to Tailwind CSS config
    Added Catppuccin theme plugin to Tailwind CSS configuration with default
    flavour set to "mocha" and prefix "ctp". This enhances the styling options
    with a new set of colors.

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -80,7 +80,14 @@ export default {
   		}
   	}
   },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@catppuccin/tailwindcss")({
+      // prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+      // default is `false`, which means no prefix
+      prefix: "ctp",
+      // which flavour of colours to use by default, in the `:root`
+      defaultFlavour: "mocha",
+    }),
+  ],
 } satisfies Config;
