26a4c5d9329ce3a7903d871a522ca5daa826004d
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-22 18:46:53 +0900

    fix: remove unused imports from AddTodoForm.tsx
    Removed unused imports FormDescription and FormMessage from the
    AddTodoForm.tsx file to clean up the code and improve readability.
    
    No functional changes were made.

--- frontend/src/app/components/AddTodoForm.tsx
+++ frontend/src/app/components/AddTodoForm.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import SelectBox from "./SelectBox";
 import { SelectItemValue } from "./SelectBox";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
